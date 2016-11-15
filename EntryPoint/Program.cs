e2a90dbef03a4030249c708cda52710d6d603a18
Remco Visser <rvremco@hotmail.com>
2016-11-15 20:08:53 +0100

    first commit

---
+++ EntryPoint/Program.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntryPoint
+{
+#if WINDOWS || LINUX
+  public static class Program
+  {
+    static void CrashMe(int n)
+    {
+      Console.Write("Going strong at level " + n + "\r                       ");
+     	CrashMe(n + 1);
+    }
+
+    [STAThread]
+     static void Main()
+	{
+		//CrashMe(0);
+		var fullscreen = false;
+		var level = "4";
+		if (level == "1")
+		{
+			var game = VirtualCity.RunAssignment1(SortSpecialBuildingsByDistance, fullscreen);
+			game.Run();
+		}
+		else if (level == "2")
+		{
+			var game = VirtualCity.RunAssignment2(FindSpecialBuildingsWithinDistanceFromHouse, fullscreen);
+			game.Run();
+		}
+		else if (level == "3")
+		{
+			var game = VirtualCity.RunAssignment3(FindRoute, fullscreen);
+			game.Run();
+		}
+		else if (level == "4")
+		{
+			var game = VirtualCity.RunAssignment4(FindRoutesToAll, fullscreen);
+			game.Run();
+		}
+	}
+
+    private static IEnumerable<Vector2> SortSpecialBuildingsByDistance(Vector2 house, IEnumerable<Vector2> specialBuildings)
+    {
+      return specialBuildings.OrderBy(v => Vector2.Distance(v, house));
+    }
+
+    private static IEnumerable<IEnumerable<Vector2>> FindSpecialBuildingsWithinDistanceFromHouse(
+      IEnumerable<Vector2> specialBuildings, 
+      IEnumerable<Tuple<Vector2, float>> housesAndDistances)
+    {
+      return
+          from h in housesAndDistances
+          select
+            from s in specialBuildings
+            where Vector2.Distance(h.Item1, s) <= h.Item2
+            select s;
+    }
+
+    private static IEnumerable<Tuple<Vector2, Vector2>> FindRoute(Vector2 startingBuilding, 
+      Vector2 destinationBuilding, IEnumerable<Tuple<Vector2, Vector2>> roads)
+    {
+      var startingRoad = roads.Where(x => x.Item1.Equals(startingBuilding)).First();
+      List<Tuple<Vector2, Vector2>> fakeBestPath = new List<Tuple<Vector2, Vector2>>() { startingRoad };
+      var prevRoad = startingRoad;
+      for (int i = 0; i < 30; i++)
+      {
+        prevRoad = (roads.Where(x => x.Item1.Equals(prevRoad.Item2)).OrderBy(x => Vector2.Distance(x.Item2, destinationBuilding)).First());
+        fakeBestPath.Add(prevRoad);
+      }
+      return fakeBestPath;
+    }
+
+    private static IEnumerable<IEnumerable<Tuple<Vector2, Vector2>>> FindRoutesToAll(Vector2 startingBuilding, 
+      IEnumerable<Vector2> destinationBuildings, IEnumerable<Tuple<Vector2, Vector2>> roads)
+    {
+      List<List<Tuple<Vector2, Vector2>>> result = new List<List<Tuple<Vector2, Vector2>>>();
+      foreach (var d in destinationBuildings)
+      {
+        var startingRoad = roads.Where(x => x.Item1.Equals(startingBuilding)).First();
+        List<Tuple<Vector2, Vector2>> fakeBestPath = new List<Tuple<Vector2, Vector2>>() { startingRoad };
+        var prevRoad = startingRoad;
+        for (int i = 0; i < 30; i++)
+        {
+          prevRoad = (roads.Where(x => x.Item1.Equals(prevRoad.Item2)).OrderBy(x => Vector2.Distance(x.Item2, d)).First());
+          fakeBestPath.Add(prevRoad);
+        }
+        result.Add(fakeBestPath);
+      }
+      return result;
+    }
+  }
+#endif
+}
